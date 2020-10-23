51afe9f855c669caff1e364296973bf66d5623b6
dyun03 <73308177+dyun03@users.noreply.github.com>
2020-10-22 23:04:47 -0700

    Update helloworld.java
    Added a comment saying it prints Hello World and changed the W into a capital.

--- helloworld.java
+++ helloworld.java
@@ -15,7 +15,7 @@ public class helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello World"); // prints Hello World
     }
     
 }
